fdf11a8537f5190d470caee133cd83ba91a109fd
Ajmal <ajmal.poovanath@gmail.com>
2025-01-06 17:36:58 +0530

    Create home view

--- Boredomo/BoredomoApp.swift
+++ Boredomo/BoredomoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BoredomoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ Boredomo/Views/HomeView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Boredomo
+//
+//  Created by Ajmal Poovanath on 06/01/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack{
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                VStack(alignment: .center, spacing: 60.0){
+                    Image("Logo")
+                        .resizable()
+                        .frame(width: 80.0, height: 80.0)
+                    
+                    VStack(spacing: 25.0){
+                        Text("BORED ?")
+                            .fontWeight(.semibold)
+                            .multilineTextAlignment(.center)
+                            .foregroundStyle(Color.white)
+                            .font(.custom("SF Pro", size: 60))
+                        Text("let's find something to do..")
+                            .fontWeight(.light)
+                            .multilineTextAlignment(.center)
+                            .foregroundStyle(Color.white)
+                            .font(.custom("SF Pro", size: 22))
+                    }
+                }
+                .padding(.top, 30.0)
+                
+                
+                Spacer()
+                    
+                //Find Button 
+                    
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
