c16b7e8634dc4446bad7292f5600bcb2460c4428
Ajmal <ajmal.poovanath@gmail.com>
2025-01-06 17:01:28 +0530

    Setup xcode project

---
+++ Boredomo/BoredomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoredomoApp.swift
+//  Boredomo
+//
+//  Created by Ajmal Poovanath on 06/01/25.
+//
+
+import SwiftUI
+
+@main
+struct BoredomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
